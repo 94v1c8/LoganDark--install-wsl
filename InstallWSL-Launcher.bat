29c7eebd0bf9c0a0af7056f6155b3fa745ddebf2
LoganDark <logandark@noreply.example.org>
2021-05-12 07:20:37 +0200

    WHOOPS

--- InstallWSL-Launcher.bat
+++ InstallWSL-Launcher.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell.exe -ExecutionPolicy ByPass "Import-Module %~dp0installWSL-2.psm1; Install-WSLInteractive; pause"
+powershell.exe -ExecutionPolicy ByPass "Import-Module %~dp0InstallWSL.psm1; Install-WSLInteractive; pause"
